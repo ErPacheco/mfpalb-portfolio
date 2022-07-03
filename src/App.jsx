88eccbd1194c61a5eed97848776cce6124b33142
Mario Fernández-Pacheco Albandea <mario.fpalb@gmail.com>
2022-07-03 19:04:21 +0200

    add dependencies and first render element

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='heading__one'>React application!!!</h1>
+    </div>
+  );
+}
